4575b1a9a23d2fd59c3f60be209cad3c35f29a31
Fran Fitzpatrick <francis.x.fitzpatrick@gmail.com>
2015-06-27 17:52:29 -0500

    Changed some arguments and argument descriptions

--- wordcounter.py
+++ wordcounter.py
@@ -1,16 +1,23 @@
 import argparse
 import logging
+import multiprocessing
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file", type=argparse.FileType('r'), nargs='+',
+description = str("A distributed, multiprocess application that will return the"
+                  " most commonly used words in a file (or combination of many "
+                  "files.")
+
+logger = logger.getLogger(__name__)
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("filename", type=argparse.FileType('r'), nargs='+',
                     help="File to perform word count on")
 parser.add_argument("-c", "--count", help="How many words to display "
                     "(default: %(default)s)", type=int, default=10)
 parser.add_argument("-re", "--regex", help="Custom regular expression",
                     default="(\w+)")
 parser.add_argument("-p", "--processes", help="The number of local "
-                    " processes to spawn (default: %(default)s).",
-                    default=3, type=int)
+                    " processes to spawn. Defaults to how many available "
+                    "CPU cores on the current computer (%(default)s).",
+                    default=multiprocessing.cpu_count(), type=int)
 args = parser.parse_args()
 
-logger = logger.getLogger(__name__)
+
