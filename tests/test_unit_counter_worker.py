9781c50fb3cacdaa988afe60afd855a881aacd95
Fran Fitzpatrick <francis.x.fitzpatrick@gmail.com>
2015-06-28 09:25:23 -0500

    renamed tests

---
+++ tests/test_unit_counter_worker.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.insert(0, "../lib")
+
+import unittest
+import Queue
+from mp_handler import MP_Handler
+from collections import Counter
+
+class Counter_Worker_Tests(unittest.TestCase):
+    def setUp(self):
+        self.handler = MP_Handler()
+        self.regex = "(\w+)" #matches words
+
+    def test_counter_worker_one_file_returns_one_result(self):
+        job_queue = Queue.Queue()
+        result_queue = Queue.Queue()
+
+        file1 = "books/test_map_three_words.txt"
+        job_queue.put([open(file1).read(), self.regex])
+
+        self.handler.counter_worker(job_queue, result_queue)
+        self.assertEquals(result_queue.qsize(), 1)
+
+    def test_counter_worker_three_files_returns_three_results(self):
+        job_queue = Queue.Queue()
+        result_queue = Queue.Queue()
+
+        file1 = "books/test_map_three_words.txt"
+        file2 = "books/test_map_three_words_with_punctuation.txt"
+        file3 = "books/test_map_sentence.txt"
+        job_queue.put([open(file1).read(), self.regex])
+        job_queue.put([open(file2).read(), self.regex])
+        job_queue.put([open(file3).read(), self.regex])
+
+        self.handler.counter_worker(job_queue, result_queue)
+        self.assertEquals(result_queue.qsize(), 3)
+
+    def test_counter_worker_three_words_expected_results_correct(self):
+        job_queue = Queue.Queue()
+        result_queue = Queue.Queue()
+        expected = Counter(['how','you','are'])
+
+        file1 = "books/test_map_three_words.txt"
+
+        job_queue.put([open(file1).read(), self.regex])
+
+        self.handler.counter_worker(job_queue, result_queue)
+
+        self.assertEquals(result_queue.get(), expected)
+
+        
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ tests/test_unit_word_count.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.insert(0, "../lib")
+
+import unittest
+from collections import Counter
+from mp_handler import MP_Handler
+
+
+class Word_Count_Tests(unittest.TestCase):
+    def setUp(self):
+        self.handler = MP_Handler()
+
+    def test_word_count_with_three_words(self):
+        regex = "(\w+)"
+        expected = Counter(['how','you','are'])
+        filename = "books/test_map_three_words.txt"
+        result = self.handler.word_count(open(filename).read(),
+                                            regex)
+
+        self.assertEquals(expected, result)
+
+    def test_word_count_with_three_words_with_punctuation(self):
+        regex = "(\w+)"
+        expected = Counter(['how','you','are'])
+        filename = "books/test_map_three_words_with_punctuation.txt"
+        result = self.handler.word_count(open(filename).read(),
+                                            regex)
+
+        self.assertEquals(expected, result)
+
+    def test_word_count_sentence(self):
+        regex = "(\w+)"
+        expected = Counter({'i': 2, 'is': 2, 'hello': 2, 'friend': 2,
+                            'said': 1, 'name': 1, 'her': 1, 'fran': 1,
+                            'there': 1, 'well': 1, 'your': 1, 'doing': 1,
+                            'jaime': 1, 'hope': 1, 'tell': 1, 'my': 1})
+        filename = "books/test_map_sentence.txt"
+        result = self.handler.word_count(open(filename).read(),
+                                            regex)
+
+        self.assertEquals(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
+
