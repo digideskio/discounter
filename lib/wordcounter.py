4f2700ab98e2711cea1924f3ac571ad9c2476a00
Fran Fitzpatrick <francis.x.fitzpatrick@gmail.com>
2015-06-28 14:15:24 -0500

    woot woot

--- lib/wordcounter.py
+++ lib/wordcounter.py
@@ -5,32 +5,41 @@ import Queue
 from collections import Counter
 
 class WordCounter():
+    def __init__(self):
+        self.mp = mp_handler.MP_Handler()
+
     def count(self, files, **kwargs):
-        mp = mp_handler.MP_Handler()
-        server_manager = mp.make_server_manager("127.0.0.1", 45329, "test")
-        shared_job_queue = server_manager.get_job_queue()
-        shared_result_queue = server_manager.get_result_queue()
+        # If it is a worker, go do work!
+        if (kwargs["worker"]):
+            self.setup_clients(kwargs["ipaddr"],
+                               kwargs["port"],
+                               kwargs["authkey"],
+                               kwargs["nprocs"])
+            return
 
-        # TODO: Chunk this up, instead of whole file
-        total_jobs = 0
-        for f in files:
-            shared_job_queue.put([f.read(), kwargs["regex"]])
-            total_jobs = total_jobs + 1
+        total_jobs = self.setup_server(kwargs["ipaddr"],
+                                       kwargs["port"],
+                                       kwargs["authkey"],
+                                       files,
+                                       kwargs["regex"])
 
-        client_manager = mp.make_client_manager("127.0.0.1", 45329, "test")
-        mp.mp_counter(client_manager.get_job_queue(), 
-                      client_manager.get_result_queue(),
-                      kwargs["nprocs"])        
+        self.setup_clients(kwargs["ipaddr"],
+                           kwargs["port"],
+                           kwargs["authkey"],
+                           kwargs["nprocs"])
 
+        shared_job_queue = self.server_manager.get_job_queue()
+        shared_result_queue = self.server_manager.get_result_queue()
         while shared_result_queue.qsize() != total_jobs:
             pass            
 
         logging.debug("Items in job queue: %d" % shared_job_queue.qsize())
         logging.debug("Items in result queue: %d" % shared_result_queue.qsize())
 
-        self.print_results(self.sum_results(shared_result_queue).most_common(kwargs["amt_of_words"]))
+        self.print_results(self.sum_results(shared_result_queue)
+                           .most_common(kwargs["amt_of_words"]))
 
-        server_manager.shutdown()
+        self.server_manager.shutdown()
 
     def sum_results(self, result_queue):
         result = Counter()
@@ -41,3 +50,31 @@ class WordCounter():
     def print_results(self, pop_words):
         for i in pop_words:
             print "%s %d" % (i[0], i[1])
+    
+    def setup_server(self, ip, port, authkey, files, regex):
+        """
+        Sets up the server, loads it with jobs, and returns how many
+        jobs are in the queue.
+        """
+        self.server_manager = self.mp.make_server_manager(ip, port, authkey)
+        shared_job_queue = self.server_manager.get_job_queue()
+        
+        # TODO: Chunk this up, instead of whole file
+        total_jobs = 0
+        for f in files:
+            shared_job_queue.put([f.read(), regex])
+            total_jobs = total_jobs + 1
+        logging.debug("Total jobs in job queue: %s" % total_jobs)
+        return total_jobs
+
+    def setup_clients(self, ip, port, authkey, nprocs):
+        """
+        Sets up the clients/workers, and connects them to the given server
+        to begin running jobs
+        """
+        self.client_manager = self.mp.make_client_manager(ip,
+                                                          port,
+                                                          authkey)
+        self.mp.mp_counter(self.client_manager.get_job_queue(), 
+                           self.client_manager.get_result_queue(),
+                           nprocs)        
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python
+
+"""Application that returns the most commonly used words in text files
+
+Distributed, multiprocess application that returns a list of most commonly
+used words in a text file, or the total accross multiple files.
+"""
+
 import sys
 import argparse
 import logging
 import multiprocessing
 import lib.wordcounter
+
+__author__ = "Fran Fitzpatrick"
+__copyright__ = "Copyright (c) 2015, %s" % __author__
+__license__ = "GPLv3"
+__version__ = "1.0"
+__maintainer__ = __author__
+__email__ = "francis.x.fitzpatrick@gmail.com"
+__status__ = "Prototype"
 
 class ErrorParser(argparse.ArgumentParser):
     def error(self, message):
@@ -18,15 +34,21 @@ parser.add_argument("filename", type=argparse.FileType('r'), nargs='+',
                     help="File to perform word count on")
 parser.add_argument("-c", "--count", help="How many words to display "
                     "(default: %(default)s)", type=int, default=10)
-parser.add_argument("-p", "--processes", help="The number of local "
-                    " processes to spawn. Defaults to how many available "
+parser.add_argument("-n", "--nprocs", help="The number of local "
+                    "processes to spawn. Defaults to how many available "
                     "CPU cores on the current computer (%(default)s).",
                     default=multiprocessing.cpu_count(), type=int)
 parser.add_argument("--regex", help="Custom regular expression",
                     default="(\w+)")
 parser.add_argument("-v", "--verbose", help="Display debugging log messages", 
                     action="store_true")
-
+parser.add_argument("--ipaddr", help="IP Address", default="127.0.0.1")
+parser.add_argument("--port", help="TCP Port", default=31337, type=int)
+parser.add_argument("--authkey", default="!QAZxsw2#EDCvfr4%TGBnhy6&UJM",
+                    help="The authorization key (or password) that the server "
+                    "and workers use to communicate")
+parser.add_argument("-w", "--worker", action="store_true",
+                    help="Start as just a worker")
 args = parser.parse_args()
 
 if (args.verbose):
@@ -34,4 +56,5 @@ if (args.verbose):
 
 wc = lib.wordcounter.WordCounter()
 wc.count(args.filename, amt_of_words=args.count, regex=args.regex,
-         nprocs=args.processes)
+         nprocs=args.nprocs, ipaddr=args.ipaddr, port=args.port,
+         authkey=args.authkey, worker=args.worker)
