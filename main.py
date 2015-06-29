2d45095ff8dfe4da6ef290e94a218490c8b066a7
Fran Fitzpatrick <francis.x.fitzpatrick@gmail.com>
2015-06-29 18:40:20 -0500

    main.py pep8 compliant

--- main.py
+++ main.py
@@ -21,15 +21,16 @@ __maintainer__ = __author__
 __email__ = "francis.x.fitzpatrick@gmail.com"
 __status__ = "Prototype"
 
+
 class ErrorParser(argparse.ArgumentParser):
     def error(self, message):
         logging.error("error: %s" % message)
         self.print_help()
         sys.exit(2)
 
-description = str("A distributed, multiprocess application that will return the"
-                  " most commonly used words in a file (or combination of many "
-                  "files).")
+description = str("A distributed, multiprocess application that will return "
+                  "the most commonly used words in a file (or combination of "
+                  "many files).")
 parser = ErrorParser(description=description)
 parser.add_argument("-f", "--file", type=argparse.FileType('r'), nargs='*',
                     help="File to perform word count on")
@@ -44,7 +45,7 @@ parser.add_argument("-n", "--nprocs", help="The number of local "
                     default=multiprocessing.cpu_count(), type=int)
 parser.add_argument("--regex", help="Custom regular expression",
                     default="(\w+)")
-parser.add_argument("-d", "--debug", help="Enables debugging mode", 
+parser.add_argument("-d", "--debug", help="Enables debugging mode",
                     action="store_true")
 parser.add_argument("--ipaddr", help="IP Address", default="127.0.0.1")
 parser.add_argument("--port", help="TCP Port", default=31337, type=int)
@@ -57,12 +58,12 @@ args = parser.parse_args()
 if (args.debug):
     logging.basicConfig(level=logging.DEBUG)
 
-if (args.worker == False and args.file == None):
+if (args.worker is False and args.file is None):
     logging.error("You must run this application with files (-f) to process or"
                   " as a worker node (-w) for distributed programming.")
     parser.print_help()
     sys.exit(1)
-elif (args.worker == True and args.file != None):
+elif (args.worker is True and args.file is not None):
     logging.error("You cannot run this application with both the -f and -w "
                   "at the same time.")
     sys.exit(1)
